c88335ff7466edce190705eaf9967de23fd189f8
최정혁 <jeonghyeogc01@gmaile.com>
2026-01-02 20:30:34 +0900

    refactor : findByName 코드 수정

--- src/main/java/com/example/plusproject/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/plusproject/domain/product/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByName(@NotBlank(message = "상품명을 입력해주세요") String productName);
+    Optional<Product> findByName(String productName);
 
     boolean existsByName(String name);
 
