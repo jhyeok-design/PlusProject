689bc96fc4cd30ef89fd7425257e4f7d0d22c2cb
최정혁 <jeonghyeogc01@gmaile.com>
2026-01-02 20:14:23 +0900

    feat : v2용 단일 상품명 검색

--- src/main/java/com/example/plusproject/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/plusproject/domain/product/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.example.plusproject.domain.product.repository;
 
 import com.example.plusproject.domain.product.entity.Product;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByName(String name);
+
+    Optional<Product> findByNameContaining(String name);
 }
