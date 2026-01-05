2103d4bb5465e0232aa159f4e03f9ef45fd4f6f8
최정혁 <jeonghyeogc01@gmaile.com>
2026-01-05 14:57:34 +0900

    feat : RedisConfig 생성

---
+++ src/main/java/com/example/plusproject/common/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.example.plusproject.common.config;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // ObjectMapper 커스터마이징
+        ObjectMapper mapper = new ObjectMapper()
+                // Jackson이 LocalDate, LocalDateTime을 정상적으로 JSON 변환할 수 있도록 지원
+                .registerModule(new JavaTimeModule())
+                // "2025-01-01T10:30:00" 형태의 가독성 좋은 문자열로 저장하기 위함
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                // 다양한 DTO를 하나의 RedisTemplate으로 처리하기 위함
+                .activateDefaultTyping(
+                        LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.PROPERTY
+                );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(mapper);
+
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
