4cce4257f64239751c34b10a839cf66feed6b53a
최정혁 <jeonghyeogc01@gmaile.com>
2026-01-09 11:59:38 +0900

    Revert "refactor - 클래스명 통잏"
    This reverts commit 44c10a620aff9134288250641543afe8d98e81b7.
    
    # Conflicts:
    #	src/main/java/com/example/plusproject/domain/post/repository/CustomPostRepository.java
    #	src/main/java/com/example/plusproject/domain/post/repository/PostRepository.java
    #	src/main/java/com/example/plusproject/domain/post/repository/PostRepositoryImpl.java

---
+++ src/main/java/com/example/plusproject/domain/post/repository/CustomPostRepository.java
@@ -0,0 +1,12 @@
+package com.example.plusproject.domain.post.repository;
+
+import com.example.plusproject.domain.post.model.PostDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomPostRepository {
+
+    Page<PostDto> findPostList(Pageable pageable);
+
+    Page<PostDto> searchByConditions(String keyword, String nickname, Pageable pageable);
+}
